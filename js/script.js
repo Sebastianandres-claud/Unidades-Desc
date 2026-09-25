8fbb42df1e0a1fb307667754d3c6af549af6f442
Sebastianandres-claud <samiranda@sitrans.cl>
2026-09-25 12:08:20 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -234,7 +234,7 @@ function extraerFechaReporte(text){
     }
     if (/DATOS\s+AL|OPERACI[OÓ]N/i.test(line)) {
       const matchTexto = line.match(/\d{1,2}[\/\-]\d{1,2}[\/\-]\d{2,4}\s+\d{1,2}:\d{2}(:\d{2})?(\s*[ap]\.?\s*m\.?)?/i);
-      if (matchTexto) return "DATOS AL " + matchTexto[0].toUpperCase();
+      if (matchTexto) return "Datos en " + matchTexto[0].toUpperCase();
     }
   }
   return null;
